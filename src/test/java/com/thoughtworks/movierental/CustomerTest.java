717431d952352bffaf83078fb888064e3a28148d
John Ed Alvinez <john.alvinez@thoughtworks.com>
2022-11-09 17:26:17 +1100

    [TEST] Add characterisation test for statement method
    Co-authored-by: John Do <john.do@thoughtworks.com>

--- src/test/java/com/thoughtworks/movierental/CustomerTest.java
+++ src/test/java/com/thoughtworks/movierental/CustomerTest.java
@@ -12,4 +12,18 @@ public class CustomerTest {
         Assert.assertEquals(expected, 1 + 2);
     }
 
+    @Test
+    public void statementCharacterisationTest() {
+        Movie testMovie = new Movie("Barbarian", Movie.REGULAR);
+        Rental testRental = new Rental(testMovie, 3);
+        Customer testCustomer = new Customer("John Do");
+
+        testCustomer.addRental(testRental);
+        String testStatement = testCustomer.statement();
+
+        System.out.println(testStatement);
+
+        String expectedStatement = "Rental Record for John Do\n\tBarbarian	3.5\nAmount owed is 3.5\nYou earned 1 frequent renter points";
+        Assert.assertEquals(expectedStatement, testStatement);
+    }
 }
